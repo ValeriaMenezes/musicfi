35ba519b77b8c321ed49a0d6c25cea252e93a615
Valéria <valeriamenezes022@gmail.com>
2022-08-31 01:15:10 -0300

    adiciona requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -14,7 +15,7 @@ class Header extends React.Component {
   async componentDidMount() {
     this.setState({ loading: 'carregando' });
     const response = await getUser();
-    console.log('1', response);
+    // console.log('1', response);
     this.setState({
       userName: response,
       loading: '',
@@ -30,6 +31,9 @@ class Header extends React.Component {
             <h3 data-testid="header-user-name">
               {userName.name}
             </h3>) }
+        <Link data-testid="link-to-search" to="/search"> Search </Link>
+        <Link data-testid="link-to-favorites" to="/favorites"> Favorites </Link>
+        <Link data-testid="link-to-profile" to="/profile"> Profile </Link>
       </header>
     );
   }
