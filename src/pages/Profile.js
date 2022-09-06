04d15d6528d859464df95028109ad49db6c40eaf
Valéria <valeriamenezes022@gmail.com>
2022-09-05 21:50:08 -0300

    adiciona requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {},
+    };
+  }
+
+  async componentDidMount() {
+    const response = await getUser();
+    // console.log(response);
+    this.setState({
+      data: response,
+    });
+  }
+
   render() {
+    const { data } = this.state;
+    const { name, email, image, description } = data;
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Profile</p>
+        <p>{name}</p>
+        <p>{email}</p>
+        <img data-testid="profile-image" src={ image } alt={ name } />
+        <p>{description}</p>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
